9ffaaf27128186d5fc58f2d5908d89d01ca14c33
merin dangol <merindangol025@gmail.com>
2026-09-10 20:54:14 +0545

    changes in requirement.txt

--- animal_rescue/settings.py
+++ animal_rescue/settings.py
@@ -29,6 +29,8 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Application definition
 
