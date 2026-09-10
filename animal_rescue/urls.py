5199daedb6dd44e587efeacbf69279cb2afbec57
merin dangol <merindangol025@gmail.com>
2026-09-10 21:22:20 +0545

    Remove all Railway hosting configuration

--- animal_rescue/urls.py
+++ animal_rescue/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('', include('rescue.urls')),
 ]
 
-# Serve media files in development AND production (Railway)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
