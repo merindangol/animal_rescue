875d041aaaa78cd97d4e883d5b8ccd1372b39f43
merin dangol <merindangol025@gmail.com>
2026-09-10 21:10:45 +0545

    Fix media and static file serving in production

--- animal_rescue/urls.py
+++ animal_rescue/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', include('rescue.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files in development AND production (Railway)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
